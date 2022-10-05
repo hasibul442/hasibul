a526bf81b663fa0526a55ede7042b9df0d40aa11
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-10-05 22:46:38 +0600

    Project Framework Install For Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-3xl">
+        This Is <samp className='font-bold underline'>Hasibul Hasan</samp>
+    </p>
+    </div>
+  );
+}
+
+export default App;
