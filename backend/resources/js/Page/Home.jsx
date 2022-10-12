cd76804aedcfe6cefa422759e5bba11bf0615ced
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2022-10-12 16:33:03 +0600

    Init Backend Framework Laravel, React

---
+++ backend/resources/js/Page/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+      <section>
+          <div>Home</div>
+          <div>Home</div>
+          <div>Home</div>
+          <div>Home</div>
+      </section>
+  );
+}
+
+export default Home
