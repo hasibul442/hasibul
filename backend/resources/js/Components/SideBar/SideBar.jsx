cf42b62d0f1cac59d9e836c013b13b7c590d9dda
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2022-11-15 18:01:32 +0600

    Dashboard Design

--- backend/resources/js/Components/SideBar/SideBar.jsx
+++ backend/resources/js/Components/SideBar/SideBar.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom';
 
@@ -5,7 +6,16 @@ function SideBar() {
 
   return (
       <>
-          
+          <div className="flex">
+              <div className={`w-72 h-screen bg-primary relative`}>
+                  <div className="absolute cursor-pointer right-2 top-5 w-7 ">
+                      <i className="fa-solid fa-bars fa-xl" style={{ color:"#fff" }}></i>
+                  </div>
+              </div>
+              <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+                  <h1>Home Page</h1>
+              </div>
+          </div>
       </>
   );
 }
--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
         "./resources/**/*.jsx",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary": "#1a202c",
+                "secondary": "#2d3748",
+                "accent": "#ed8936",
+                "accent-light": "#f6ad55",
+                "accent-dark": "#dd6b20",
+                "accent-darker": "#c05621",
+                "accent-darkest": "#9c4221",
+                "accent-lightest": "#fffaf0",
+                "accent-lighter": "#fff5e5",
+            }
+        },
     },
     plugins: [],
 };
