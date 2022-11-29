c011e0393e2c95de5ef7b97bdee03ee271f6d91d
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-11-29 23:25:52 +0600

    Dashboard

--- backend/resources/js/Page/Dashboard/Dashboard.jsx
+++ backend/resources/js/Page/Dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Lottie from 'react-lottie';
+import React from "react";
+import Lottie from "react-lottie";
 import "./dashboard.css";
-import  user from './../../../../public/assets/Icon/user.json';
-import  category from './../../../../public/assets/Icon/type.json';
-import  books from './../../../../public/assets/Icon/books.json';
+import user from "./../../../../public/assets/Icon/user.json";
+import category from "./../../../../public/assets/Icon/type.json";
+import books from "./../../../../public/assets/Icon/books.json";
 
 function Dashboard() {
     const userIcon = {
@@ -11,8 +11,8 @@ function Dashboard() {
         autoplay: true,
         animationData: user,
         rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
+            preserveAspectRatio: "xMidYMid slice",
+        },
     };
 
     const categoryIcon = {
@@ -20,8 +20,8 @@ function Dashboard() {
         autoplay: true,
         animationData: category,
         rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
+            preserveAspectRatio: "xMidYMid slice",
+        },
     };
 
     const bookIcon = {
@@ -29,48 +29,67 @@ function Dashboard() {
         autoplay: true,
         animationData: books,
         rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
+            preserveAspectRatio: "xMidYMid slice",
+        },
     };
-  return (
-    <>
-        <div className="grid grid-cols-3 gap-3">
-            <div className="card-1 rounded-md">
-                <div className="flex">
-                    <div className=" flex-1 py-2 text-center">
-                        <Lottie options={userIcon} height={120} width={120}/>
+    return (
+        <>
+            <section>
+                <div className="pt-1 mb-1">
+                    <h5 className="text-light-1 text-2xl">Books Project</h5>
+                </div>
+                <div className="grid grid-cols-3 gap-3 pt-1">
+                    <div className="card-1 rounded-md">
+                        <div className="flex">
+                            <div className=" flex-1 py-2 text-center">
+                                <Lottie
+                                    options={userIcon}
+                                    height={120}
+                                    width={120}
+                                />
+                            </div>
+                            <div className="text-dark-1 flex-1 text-center">
+                                <p className="text-xl font-bold pt-8">Users</p>
+                                <p className="text-2xl font-bold ">10</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="text-dark-1 flex-1 text-center">
-                        <p className="text-2xl font-bold pt-14">200</p>
+
+                    <div className="card-2 rounded-md">
+                        <div className="flex">
+                            <div className=" flex-1 py-2 text-center">
+                                <Lottie
+                                    options={categoryIcon}
+                                    height={120}
+                                    width={120}
+                                />
+                            </div>
+                            <div className="text-dark-1 flex-1 text-center">
+                                <p className="text-xl font-bold pt-8">Category</p>
+                                <p className="text-2xl font-bold ">20</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card-3 rounded-md">
+                        <div className="flex">
+                            <div className=" flex-1 py-2 text-center">
+                                <Lottie
+                                    options={bookIcon}
+                                    height={120}
+                                    width={120}
+                                />
+                            </div>
+                            <div className="text-dark-1 flex-1 text-center">
+                                <p className="text-xl font-bold pt-8">Books</p>
+                                <p className="text-2xl font-bold ">10</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div className="card-2 rounded-md">
-                <div className="flex">
-                    <div className=" flex-1 py-2 text-center">
-                        <Lottie options={categoryIcon} height={120} width={120}/>
-                    </div>
-                    <div className="text-dark-1 flex-1 text-center">
-                    <p className="text-2xl font-bold pt-14">200</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card-3 rounded-md">
-                <div className="flex">
-                    <div className=" flex-1 py-2 text-center">
-                        <Lottie options={bookIcon} height={120} width={120}/>
-                    </div>
-                    <div className="text-dark-1 flex-1 text-center">
-                    <p className="text-2xl font-bold pt-14">200</p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </>
-  )
+            </section>
+        </>
+    );
 }
 
-export default Dashboard
+export default Dashboard;
