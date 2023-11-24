9475c31f79b6503372d47a08e4583869433941b7
Hasibul Hasan <hasibulhasan442@gmail.com>
2023-11-24 08:00:10 +0000

    Update mobile navigation toggle class name

--- src/pages/NavBar/TopBar.jsx
+++ src/pages/NavBar/TopBar.jsx
@@ -14,7 +14,7 @@ function TopBar() {
                     <li><Link className="nav-link" to="/portfolio">Portfolio</Link></li>
                     <li><Link className="nav-link" to="/contact">Contact</Link></li>
                 </ul>
-                <i className="bi bi-list mobile-nav-toggle"></i>
+                <i className={`${style.mobile_nav_toggle} bi bi-list `}></i>
             </nav>
         </>
     )
