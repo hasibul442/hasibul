b8e2eeaa2da4b4c96bee5bd5c8b4a3a1b1e49b97
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-07-16 23:54:00 +0600

    Refactor TopBar2 navigation: comment out Resume link and ensure Experience link is active

--- src/pages/NavBar/TopBar2.jsx
+++ src/pages/NavBar/TopBar2.jsx
@@ -17,8 +17,9 @@ function TopBar2() {
                         <ul className="navbar-nav ms-auto">
                             <li><Link className="nav-link" to="/">Home</Link></li>
                             <li><Link className="nav-link" to="/about">About</Link></li>
-                            <li><Link className="nav-link" to="/resume">Resume</Link></li>
+                            {/* <li><Link className="nav-link" to="/resume">Resume</Link></li> */}
                             {/* <li><Link className="nav-link" to="/services">Services</Link></li> */}
+                            <li><Link className="nav-link" to="/experience">Experience</Link></li>
                             <li><Link className="nav-link" to="/portfolio">Portfolio</Link></li>
                             <li><Link className="nav-link" to="/contact">Contact</Link></li>
                             <li><Link className="nav-link" to="/blogs">Blogs</Link></li>
