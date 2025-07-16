ee1e5388c18134c30965a4e09281d94b60cea080
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-07-16 21:30:01 +0600

    Refactor TopBar2 component to improve structure and enhance branding with logo integration

--- src/pages/NavBar/TopBar2.jsx
+++ src/pages/NavBar/TopBar2.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import style from './topbar2.module.css'
 function TopBar2() {
     return (
-       <div className={style.navBar_Background}>
-         <div className='container'>
-            <nav className={`${style.navbar1} navbar navbar-expand-lg`} id="navbar">
-                <h1><a className="navbar-brand text-light" href="/">Hasibul Hasan</a></h1>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav ms-auto">
-                        <li><Link className="nav-link" to="/">Home</Link></li>
-                        <li><Link className="nav-link" to="/about">About</Link></li>
-                        <li><Link className="nav-link" to="/resume">Resume</Link></li>
-                        <li><Link className="nav-link" to="/services">Services</Link></li>
-                        <li><Link className="nav-link" to="/portfolio">Portfolio</Link></li>
-                        <li><Link className="nav-link" to="/contact">Contact</Link></li>
-                        <li><Link className="nav-link" to="/blogs">Blogs</Link></li>
+        <div className={style.navBar_Background}>
+            <div className='container'>
+                <nav className={`${style.navbar1} navbar navbar-expand-lg`} id="navbar">
 
-                    </ul>
-                </div>
-            </nav>
+                    <a className="navbar-brand text-light" href="/">
+                        <img src="/Logo.svg" alt="logo" style={{ height: "35px" }} />
+                    </a>
+
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav ms-auto">
+                            <li><Link className="nav-link" to="/">Home</Link></li>
+                            <li><Link className="nav-link" to="/about">About</Link></li>
+                            <li><Link className="nav-link" to="/resume">Resume</Link></li>
+                            <li><Link className="nav-link" to="/services">Services</Link></li>
+                            <li><Link className="nav-link" to="/portfolio">Portfolio</Link></li>
+                            <li><Link className="nav-link" to="/contact">Contact</Link></li>
+                            <li><Link className="nav-link" to="/blogs">Blogs</Link></li>
+
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </div>
-       </div>
     )
 }
 
