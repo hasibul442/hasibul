24f23241c45421fa81cc3eb6e4f0c32b26e2d70c
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-07-16 21:43:42 +0600

    Add Blog component with TopBar2 integration for navigation

---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TopBar2 from '../NavBar/TopBar2'
+
+function Blog() {
+    return (
+        <>
+            <div>
+                <TopBar2 />
+            </div>
+        </>
+    )
+}
+
+export default Blog
