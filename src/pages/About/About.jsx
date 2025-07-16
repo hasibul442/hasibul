a4bd87c9ff79f7cff05e0488d7b7c3305efe13b0
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-07-16 22:15:53 +0600

    Refactor About component layout for improved structure and styling

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,165 +11,166 @@ function About() {
       <div>
         <TopBar2 />
       </div>
-      <div id="about" className="about mt-5">
-        <div className="container">
-          <div className="row pt-5">
-            <div
-              className="col-lg-6 content"
-            >
-              <div className={style.section_title}>
-                <h2>About</h2>
+      <div style={{ height: "100vh" }}>
+        <div id="about" className="about mt-5 bg_1">
+          <div className="container">
+            <div className="row pt-5">
+              <div
+                className="col-lg-6 content"
+              >
+                <div className={style.section_title}>
+                  <h2>About</h2>
+                </div>
+                <p className="fst-italic">
+                  A Senior Software Engineer proficient in developing diverse
+                  microservice APIs, leading software development teams, and
+                  designing robust software architecture. Eager to acquire new
+                  skills and familiar with emerging technologies, prepared to work
+                  independently in fast-paced environments and collaboratively
+                  within a team setting. Objective is to secure a respected
+                  position to effectively utilize skills as a Computer Engineer,
+                  leveraging technical expertise in computer systems.
+                </p>
               </div>
-              <p className="fst-italic">
-                A Senior Software Engineer proficient in developing diverse
-                microservice APIs, leading software development teams, and
-                designing robust software architecture. Eager to acquire new
-                skills and familiar with emerging technologies, prepared to work
-                independently in fast-paced environments and collaboratively
-                within a team setting. Objective is to secure a respected
-                position to effectively utilize skills as a Computer Engineer,
-                leveraging technical expertise in computer systems.
-              </p>
-            </div>
-            <div
-              className="col-lg-6"
-            >
-              <Animation src="./lottie/file_1.lottie" />
-            </div>
-          </div>
-        </div>
-
-        <div className="counts container">
-          <div className="row">
-            <div className="col-lg-3 col-md-6">
-              <div className="count-box">
-                <i className="bi bi-emoji-smile"></i>
-                <span
-                  data-purecounter-start="0"
-                  data-purecounter-end="232"
-                  data-purecounter-duration="1"
-                  className="purecounter"
-                ></span>
-                <p>Happy Clients</p>
-              </div>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mt-5 mt-md-0">
-              <div className="count-box">
-                <i className="bi bi-journal-richtext"></i>
-                <span
-                  data-purecounter-start="0"
-                  data-purecounter-end="521"
-                  data-purecounter-duration="1"
-                  className="purecounter"
-                ></span>
-                <p>Projects</p>
-              </div>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mt-5 mt-lg-0">
-              <div className="count-box">
-                <i className="bi bi-headset"></i>
-                <span
-                  data-purecounter-start="0"
-                  data-purecounter-end="1463"
-                  data-purecounter-duration="1"
-                  className="purecounter"
-                ></span>
-                <p>Hours of Support</p>
-              </div>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mt-5 mt-lg-0">
-              <div className="count-box">
-                <i className="bi bi-award"></i>
-                <span
-                  data-purecounter-start="0"
-                  data-purecounter-end="24"
-                  data-purecounter-duration="1"
-                  className="purecounter"
-                ></span>
-                <p>Awards</p>
+              <div
+                className="col-lg-6"
+              >
+                <Animation src="./lottie/file_1.lottie" />
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="skills container">
-          <div className="section-title">
-            <h2>Skills</h2>
+          <div className="counts container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="count-box">
+                  <i className="bi bi-emoji-smile"></i>
+                  <span
+                    data-purecounter-start="0"
+                    data-purecounter-end="232"
+                    data-purecounter-duration="1"
+                    className="purecounter"
+                  ></span>
+                  <p>Happy Clients</p>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 mt-5 mt-md-0">
+                <div className="count-box">
+                  <i className="bi bi-journal-richtext"></i>
+                  <span
+                    data-purecounter-start="0"
+                    data-purecounter-end="521"
+                    data-purecounter-duration="1"
+                    className="purecounter"
+                  ></span>
+                  <p>Projects</p>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                <div className="count-box">
+                  <i className="bi bi-headset"></i>
+                  <span
+                    data-purecounter-start="0"
+                    data-purecounter-end="1463"
+                    data-purecounter-duration="1"
+                    className="purecounter"
+                  ></span>
+                  <p>Hours of Support</p>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                <div className="count-box">
+                  <i className="bi bi-award"></i>
+                  <span
+                    data-purecounter-start="0"
+                    data-purecounter-end="24"
+                    data-purecounter-duration="1"
+                    className="purecounter"
+                  ></span>
+                  <p>Awards</p>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <Skill />
-        </div>
+          <div className="skills container">
+            <div className="section-title">
+              <h2>Skills</h2>
+            </div>
 
-        <div className="interests container mt-5">
-          <div className="section-title">
-            <h2>Interests</h2>
+            <Skill />
           </div>
 
-          <div className="row">
-            <div className="col-lg-3 col-md-4">
-              <div className="icon-box">
-                <i className="ri-store-line" style={{ color: "#ffbb2c" }}></i>
-                <h3>Gadget</h3>
-              </div>
+          <div className="interests container mt-5">
+            <div className="section-title">
+              <h2>Interests</h2>
             </div>
-            <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
-              <div className="icon-box">
-                <i
-                  className="ri-bar-chart-box-line"
-                  style={{ color: "#5578ff" }}
-                ></i>
-                <h3>New Technology</h3>
+
+            <div className="row">
+              <div className="col-lg-3 col-md-4">
+                <div className="icon-box">
+                  <i className="ri-store-line" style={{ color: "#ffbb2c" }}></i>
+                  <h3>Gadget</h3>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
-              <div className="icon-box">
-                <i
-                  className="ri-calendar-todo-line"
-                  style={{ color: "#e80368" }}
-                ></i>
-                <h3>Arduino</h3>
+              <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
+                <div className="icon-box">
+                  <i
+                    className="ri-bar-chart-box-line"
+                    style={{ color: "#5578ff" }}
+                  ></i>
+                  <h3>New Technology</h3>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3 col-md-4 mt-4 mt-lg-0">
-              <div className="icon-box">
-                <i
-                  className="ri-paint-brush-line"
-                  style={{ color: "#e361ff" }}
-                ></i>
-                <h3>Artificial Intelligence</h3>
+              <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
+                <div className="icon-box">
+                  <i
+                    className="ri-calendar-todo-line"
+                    style={{ color: "#e80368" }}
+                  ></i>
+                  <h3>Arduino</h3>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3 col-md-4 mt-4">
-              <div className="icon-box">
-                <i
-                  className="ri-database-2-line"
-                  style={{ color: "#47aeff" }}
-                ></i>
-                <h3>Cloud Computing</h3>
+              <div className="col-lg-3 col-md-4 mt-4 mt-lg-0">
+                <div className="icon-box">
+                  <i
+                    className="ri-paint-brush-line"
+                    style={{ color: "#e361ff" }}
+                  ></i>
+                  <h3>Artificial Intelligence</h3>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3 col-md-4 mt-4">
-              <div className="icon-box">
-                <i
-                  className="ri-gradienter-line"
-                  style={{ color: "#ffa76e" }}
-                ></i>
-                <h3>Natural Language Processing </h3>
+              <div className="col-lg-3 col-md-4 mt-4">
+                <div className="icon-box">
+                  <i
+                    className="ri-database-2-line"
+                    style={{ color: "#47aeff" }}
+                  ></i>
+                  <h3>Cloud Computing</h3>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3 col-md-4 mt-4">
-              <div className="icon-box">
-                <i
-                  className="ri-file-list-3-line"
-                  style={{ color: "#11dbcf" }}
-                ></i>
-                <h3>DevOps</h3>
+              <div className="col-lg-3 col-md-4 mt-4">
+                <div className="icon-box">
+                  <i
+                    className="ri-gradienter-line"
+                    style={{ color: "#ffa76e" }}
+                  ></i>
+                  <h3>Natural Language Processing </h3>
+                </div>
               </div>
-            </div>
-            {/* <div className="col-lg-3 col-md-4 mt-4">
+              <div className="col-lg-3 col-md-4 mt-4">
+                <div className="icon-box">
+                  <i
+                    className="ri-file-list-3-line"
+                    style={{ color: "#11dbcf" }}
+                  ></i>
+                  <h3>DevOps</h3>
+                </div>
+              </div>
+              {/* <div className="col-lg-3 col-md-4 mt-4">
               <div className="icon-box">
                 <i
                   className="ri-price-tag-2-line"
@@ -178,36 +179,37 @@ function About() {
                 <h3>Pira Neve</h3>
               </div>
             </div> */}
-            {/* <div className="col-lg-3 col-md-4 mt-4">
+              {/* <div className="col-lg-3 col-md-4 mt-4">
               <div className="icon-box">
                 <i className="ri-anchor-line" style={{ color: "#b2904f" }}></i>
                 <h3>Dirada Pack</h3>
               </div>
             </div> */}
-            <div className="col-lg-3 col-md-4 mt-4">
-              <div className="icon-box">
-                <i className="ri-disc-line" style={{ color: "#b20969" }}></i>
-                <h3>Listening Music</h3>
+              <div className="col-lg-3 col-md-4 mt-4">
+                <div className="icon-box">
+                  <i className="ri-disc-line" style={{ color: "#b20969" }}></i>
+                  <h3>Listening Music</h3>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3 col-md-4 mt-4">
-              <div className="icon-box">
-                <i
-                  className="ri-base-station-line"
-                  style={{ color: "#ff5828" }}
-                ></i>
-                <h3>Fish Aquarium</h3>
+              <div className="col-lg-3 col-md-4 mt-4">
+                <div className="icon-box">
+                  <i
+                    className="ri-base-station-line"
+                    style={{ color: "#ff5828" }}
+                  ></i>
+                  <h3>Fish Aquarium</h3>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="testimonials container mt-5 mb-5">
-          <div className="section-title">
-            <h2>Testimonials</h2>
+          <div className="testimonials container mt-5 pb-5">
+            <div className="section-title">
+              <h2>Testimonials</h2>
+            </div>
+
+            <Testimonials />
           </div>
-
-          <Testimonials />
         </div>
       </div>
     </>
