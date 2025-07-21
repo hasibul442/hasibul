faae6e2888fd2013fc7b49919ed1ceb2f56596fa
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-07-21 20:13:18 +0600

    Remove All

--- resources/js/App.jsx
+++
@@ -1,7 +0,0 @@
-
-import "./bootstrap";
-import "../css/app.css";
-import ReactDOM from "react-dom/client";
-import Master from "./Master";
-
-ReactDOM.createRoot(document.getElementById("app")).render(<Master />);
--- resources/js/index.jsx
+++
@@ -1,6 +0,0 @@
-import "./bootstrap";
-import "../css/app.css";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("app")).render(<App />);
