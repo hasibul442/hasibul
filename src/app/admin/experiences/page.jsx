e317678a74ce8c0277130b03abca8600198ceae8
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-07-24 20:06:46 +0600

    feat: replace confirm dialog with sweetalert2 for experience deletion confirmation and notifications

--- src/app/admin/experiences/page.jsx
+++ src/app/admin/experiences/page.jsx
@@ -6,6 +6,7 @@ import {
 import { calculateDateDifference } from "@/Helper/Helper";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 function Page() {
   const [experiences, setExperiences] = useState([]);
@@ -18,16 +19,34 @@ function Page() {
   };
 
   const deleteExperience = async (id) => {
-    if (confirm("Are you sure you want to delete this experience?")) {
-      deleteDataFromDatabase("experiences", id)
-        .then(() => {
-          getExperiences();
-        })
-        .catch((error) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteDataFromDatabase("experiences", id).then(() => {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your experience has been deleted.",
+            icon: "success",
+          }).then(() => {
+            getExperiences();
+          });
+        }).catch((error) => {
           console.error("Error deleting experience:", error);
+          Swal.fire({
+            title: "Error!",
+            text: "There was an error deleting the experience.",
+            icon: "error",
+          });
         });
-      console.log("Experience deleted successfully.");
-    }
+      }
+    });
   };
 
   useEffect(() => {
