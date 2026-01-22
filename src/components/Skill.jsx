0d68f297641f2e974a6b4c5e8c98a011e9a071cd
Hasibul Hasan <hasibulhasan442@gmail.com>
2026-01-22 21:56:19 +0600

    Refactor Skill component to organize skills into orbital rings and enhance styling with solar system theme

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -19,13 +19,14 @@ import {
   FaWhatsapp,
 } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
-import { SiAdobexd, SiFirebase, SiMysql, SiNextdotjs } from "react-icons/si";
+import { SiAdobexd, SiFirebase, SiMysql, SiNextdotjs, SiMongodb } from "react-icons/si";
 import { TbApi, TbBrandJavascript } from "react-icons/tb";
 
 import style from './component.module.css'
 
 function Skill() {
-  const data = [
+  // Organize skills into 4 orbital rings
+  const orbit1 = [
     {
       id: 1,
       title: "HTML",
@@ -39,16 +40,16 @@ function Skill() {
       icon: <FaCss3Alt size={40} color="#2965f1" />,
     },
     {
-      id: 3,
-      title: "Bootstrap",
-      value: 90,
-      icon: <FaBootstrap size={40} color="#ff0000" />,
-    },
-    {
       id: 4,
       title: "JavaScript",
       value: 80,
       icon: <TbBrandJavascript size={40} color="#f0db4f" />,
+    },
+    {
+      id: 3,
+      title: "Bootstrap",
+      value: 90,
+      icon: <FaBootstrap size={40} color="#ff0000" />,
     },
     {
       id: 5,
@@ -56,6 +57,9 @@ function Skill() {
       value: 70,
       icon: <DiJqueryLogo size={40} color="#FF0000" />,
     },
+  ];
+
+  const orbit2 = [
     {
       id: 6,
       title: "React",
@@ -75,18 +79,6 @@ function Skill() {
       icon: <FaReact size={40} color="#61DBFB" />,
     },
     {
-      id: 8,
-      title: "Java",
-      value: 70,
-      icon: <FaJava size={40} color="#F80000" />,
-    },
-    {
-      id: 9,
-      title: "Spring Boot",
-      value: 60,
-      icon: <BiLogoSpringBoot size={40} color="#6DB33F" />,
-    },
-    {
       id: 10,
       title: "PHP",
       value: 80,
@@ -97,6 +89,21 @@ function Skill() {
       title: "Laravel",
       value: 70,
       icon: <FaLaravel size={40} color="#F05340" />,
+    },
+  ];
+
+  const orbit3 = [
+    {
+      id: 8,
+      title: "Java",
+      value: 70,
+      icon: <FaJava size={40} color="#F80000" />,
+    },
+    {
+      id: 9,
+      title: "Spring Boot",
+      value: 60,
+      icon: <BiLogoSpringBoot size={40} color="#6DB33F" />,
     },
     {
       id: 12,
@@ -111,8 +118,23 @@ function Skill() {
       icon: <SiFirebase size={40} color="#FFCA28" />,
     },
     {
+      id: 20,
+      title: "MongoDB",
+      value: 85,
+      icon: <SiMongodb size={40} color="#28A745" />,
+    },
+  ];
+
+  const orbit4 = [
+    {
+      id: 17,
+      title: "Python",
+      value: 70,
+      icon: <FaPython size={40} color="#FFD43B" />,
+    },
+    {
       id: 14,
-      title: "Relational Database",
+      title: "Database",
       value: 80,
       icon: <FaDatabase size={40} color="#0078D4" />,
     },
@@ -128,53 +150,63 @@ function Skill() {
       value: 80,
       icon: <FaGit size={40} color="#F05032" />,
     },
-    {
-      id: 17,
-      title: "Python",
-      value: 70,
-      icon: <FaPython size={40} color="#FFD43B" />,
-    },
-    {
-      id: 19,
-      title: "Adobe XD",
-      value: 85,
-      icon: <SiAdobexd size={40} color="#FF26BE" />,
-    },
-    {
-      id: 20,
-      title: "Figma",
-      value: 85,
-      icon: <FaFigma size={40} color="#9747FF" />,
-    },
   ];
+
+  const renderOrbitSkills = (skills, orbitClass) => {
+    const angleStep = 360 / skills.length;
+    return skills.map((skill, index) => {
+      const angle = index * angleStep;
+      return (
+        <div
+          key={skill.id}
+          className={`${style.orbit_item} ${style[orbitClass]}`}
+          style={{
+            transform: `rotate(${angle}deg) translateX(var(--orbit-radius)) rotate(-${angle}deg)`
+          }}
+        >
+          <div className={style.skill_planet}>
+            {skill.icon}
+            <span className={style.skill_name}>{skill.title}</span>
+          </div>
+        </div>
+      );
+    });
+  };
   return (
-    <>
-      {/* <div className={style.card_wrap}>
-        <div className={`${style.card_header} ${style.one}`}>
-          <i className="fas fa-code"></i>
+    <div className={style.solar_system_container}>
+      <div className={style.solar_system}>
+        {/* Central Sun */}
+        <div className={style.sun}>
+          <div className={style.sun_core}>
+            <span className={style.sun_text}>Skills</span>
+          </div>
         </div>
-        <div className={style.card_content}>
-          <h1 className={style.card_title}>Title</h1>
-          <p className={style.card_text}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-          <button className={`${style.card_btn} ${style.one}`}>code</button>
+
+        {/* Orbit 1 */}
+        <div className={`${style.orbit} ${style.orbit_1}`}>
+          <div className={style.orbit_ring}></div>
+          {renderOrbitSkills(orbit1, 'orbit1')}
         </div>
-      </div> */}
-      <div className="container">
-        <div className="row g-3">
-          {data.map((item) => (
-            <div className="col-6 col-md-4 col-lg-2 text-center" key={item.id}>
-              <div className={style.icon_box}>
-                {item.icon}
-                <h6>{item.title}</h6>
-              </div>
-            </div>
-          ))}
+
+        {/* Orbit 2 */}
+        <div className={`${style.orbit} ${style.orbit_2}`}>
+          <div className={style.orbit_ring}></div>
+          {renderOrbitSkills(orbit2, 'orbit2')}
+        </div>
+
+        {/* Orbit 3 */}
+        <div className={`${style.orbit} ${style.orbit_3}`}>
+          <div className={style.orbit_ring}></div>
+          {renderOrbitSkills(orbit3, 'orbit3')}
+        </div>
+
+        {/* Orbit 4 */}
+        <div className={`${style.orbit} ${style.orbit_4}`}>
+          <div className={style.orbit_ring}></div>
+          {renderOrbitSkills(orbit4, 'orbit4')}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
