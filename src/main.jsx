3a0e470e1fe4bd373e4b3cc02cfe0745be33c73a
Hasibul Hasan <hasibulhasan442@gmail.com>
2026-01-26 15:31:10 +0600

    Import skeleton CSS for loading state

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext'
+import "react-loading-skeleton/dist/skeleton.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
