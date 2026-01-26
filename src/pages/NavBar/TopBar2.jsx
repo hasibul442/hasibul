95b2b34e7428d2d86f45447f64bc82a5cdff96c9
Hasibul Hasan <hasibulhasan442@gmail.com>
2026-01-26 15:41:10 +0600

    Enhance TopBar2 with navigation and dropdown features
    Added navigation functionality with dropdown for 'About' section and smooth scrolling to hash links.

--- src/pages/NavBar/TopBar2.jsx
+++ src/pages/NavBar/TopBar2.jsx
@@ -1,35 +1,150 @@
-import { Link } from 'react-router-dom'
-import style from './topbar2.module.css'
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useTheme } from "../../context/ThemeContext";
+import style from "./topbar2.module.css";
 function TopBar2() {
-    return (
-        <div className={style.navBar_Background}>
-            <div className='container'>
-                <nav className={`${style.navbar1} navbar navbar-expand-lg`} id="navbar">
+  const navigate = useNavigate();
+  const { theme } = useTheme();
 
-                    <a className="navbar-brand text-light" href="/">
-                        <img src="/Logo.svg" alt="logo" style={{ height: "35px" }} />
-                    </a>
+  useEffect(() => {
+    // Scroll to hash on mount and hash change
+    const hash = window.location.hash;
+    if (hash) {
+      setTimeout(() => {
+        const element = document.querySelector(hash);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+      }, 100);
+    }
+  }, [window.location.hash]);
 
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                        <ul className="navbar-nav ms-auto">
-                            <li><Link className="nav-link" to="/">Home</Link></li>
-                            <li><Link className="nav-link" to="/about">About</Link></li>
-                            {/* <li><Link className="nav-link" to="/resume">Resume</Link></li> */}
-                            {/* <li><Link className="nav-link" to="/services">Services</Link></li> */}
-                            <li><Link className="nav-link" to="/experience">Experience</Link></li>
-                            <li><Link className="nav-link" to="/portfolio">Portfolio</Link></li>
-                            <li><Link className="nav-link" to="/contact">Contact</Link></li>
-                            <li><Link className="nav-link" to="/blogs">Blogs</Link></li>
+  const handleDropdownClick = (path, hash) => {
+    navigate(path);
+    setTimeout(() => {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    }, 100);
+  };
 
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    )
+  return (
+    <div className={style.navBar_Background}>
+      <div className="container">
+        <nav className={`${style.navbar1} navbar navbar-expand-lg`} id="navbar">
+          <a className="navbar-brand text-light" href="/">
+            <img src="/Logo.svg" alt="logo" style={{ height: "35px" }} />
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav ms-auto">
+              <li>
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className={`nav-item dropdown`}>
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="aboutDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  About
+                </a>
+                <div
+                  className={`dropdown-menu ${style.dropdownMenu} ${theme === "dark" ? style.darkTheme : style.lightTheme}`}
+                  aria-labelledby="aboutDropdown"
+                >
+                  <a
+                    className={`dropdown-item ${style.dropdownItem}`}
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleDropdownClick("/about", "#about-me");
+                    }}
+                  >
+                    About me
+                  </a>
+                  <a
+                    className={`dropdown-item ${style.dropdownItem}`}
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleDropdownClick("/about", "#skills");
+                    }}
+                  >
+                    Skills / Tech Stack
+                  </a>
+                  <a
+                    className={`dropdown-item ${style.dropdownItem}`}
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleDropdownClick("/about", "#interests");
+                    }}
+                  >
+                    Interests
+                  </a>
+                  <a
+                    className={`dropdown-item ${style.dropdownItem}`}
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleDropdownClick("/about", "#testimonials");
+                    }}
+                  >
+                    Testimonials
+                  </a>
+                </div>
+              </li>
+              {/* <li><Link className="nav-link" to="/resume">Resume</Link></li> */}
+              {/* <li><Link className="nav-link" to="/services">Services</Link></li> */}
+              <li>
+                <Link className="nav-link" to="/experience">
+                  Experience
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/portfolio">
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/certifications">
+                  Certifications
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/blogs">
+                  Blogs
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
-export default TopBar2
+export default TopBar2;
