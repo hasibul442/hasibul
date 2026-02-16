2b807538192584749f854f1c5c1c984c84051d42
Hasibul Hasan <hasibulhasan442@gmail.com>
2026-02-16 14:51:04 +0600

    Fix link path for visitors in Sidebar component

--- src/app/admin/include/Sidebar.jsx
+++ src/app/admin/include/Sidebar.jsx
@@ -69,7 +69,7 @@ function Sidebar() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className={`nav-link ${pathname === "/admin/visitor" ? "active" : ""}`} href="/admin/visitor">
+                            <Link className={`nav-link ${pathname === "/admin/visitors" ? "active" : ""}`} href="/admin/visitors">
                                 <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                                     <FaIcons.FaBook size={16} />
                                 </div>
